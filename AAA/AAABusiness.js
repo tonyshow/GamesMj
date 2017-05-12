7a69e74cec848368a1fff548aa846d71bad003ce
tony <994262676@qq.com>
2017-05-12 12:04:34 +0800

    单个游戏模板

---
+++ AAA/AAABusiness.js
@@ -0,0 +1,49 @@
+
+var AAABusiness = function(){
+
+};
+
+var pro = AAABusiness.prototype;
+
+/***主游戏界面制作完成后调用
+ *  处理游戏
+ *  defaultUIFatherNode : 个性化的节点父节点载体
+ */
+pro.onInitUI = function( defaultUIFatherNode ){
+
+};
+
+/***超时处理 
+ * seatIdx:逻辑座位号
+ */
+pro.timeOut = function(){
+
+};
+
+/***抓牌*
+ * seatIdx:逻辑座位号
+ */
+pro.sendCard = function( seatIdx ){
+
+};
+
+/***出牌
+ * seatIdx:逻辑座位号
+ */
+pro.outCard = function( seatIdx ){
+
+};
+
+/***动作
+ * seatIdx:逻辑座位号
+ * enumAction:动作类型  吃、碰、杠、听...
+ * id：动作目标  (enumAction的牌)
+ */
+pro.doAction = function( seatIdx , enumAction , id ){
+
+};
+
+/***重连成功 */
+pro.resetFinsh = function(){
+
+};
--- AAA/README.md
+++ AAA/README.md
@@ -1,3 +1,5 @@
 单个游戏的模板
 
-
+AAABusiness.js 游戏业务
+AAAGameData.js 游戏数据
+AAAStart.js	   游戏启动脚本	
